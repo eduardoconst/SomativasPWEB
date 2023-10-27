d055ff2cd5d1b0b5d745bb2e597b2d1ff53644d8
Eduardo Ferreira <147551981+eduardoconst@users.noreply.github.com>
2023-10-27 20:29:41 -0300

    Adicionando Animações

---
+++ Somativa1/scripts/animation.js
@@ -0,0 +1,13 @@
+const myOberver = new IntersectionObserver( (entries) =>{
+    entries.forEach( (entry) => {
+      if (entry.isIntersecting){
+        entry.target.classList.add("show")
+      }else{
+        entry.target.classList.remove("show")
+      }
+    })
+  })
+  
+  const elements = document.querySelectorAll('.hidden')
+  
+  elements.forEach( (element) => myOberver.observe(element) )
